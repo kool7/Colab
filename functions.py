19d7e309ead07866fcdc8065c3f70757a1e70689
kool7 <40597687+kool7@users.noreply.github.com>
2019-10-18 13:35:06 +0530

    fastai functions

--- functions.py
+++ functions.py
@@ -1,2 +1,9 @@
 def train_cats(df):
-    for 
+    for n, c in df.items():
+        if is_string_dtype(c): df[n] = c.astype('category').cat.as_ordered()
+            
+            
+def add_cats(df, trn):
+    for n, c in df.items():
+        if trn[n].dtype.name == 'category':
+            df[n] = pd.Categorical(c, categories=trn[n].cat.categories, ordered=True)

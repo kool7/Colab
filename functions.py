96acc595d46eeacad3220fd23011529ccee03f47
kool7 <40597687+kool7@users.noreply.github.com>
2019-10-18 13:57:32 +0530

    is_string_dtype

--- functions.py
+++ functions.py
@@ -2,6 +2,8 @@ def train_cats(df):
     for n, c in df.items():
         if is_string_dtype(c): df[n] = c.astype('category').cat.as_ordered()
             
+# NOTE: is_string_dtype isn't working in kaggle it is part of pandas api so I imported it as follows in kaggle ,
+# pandas.api.types.is_string_type 
             
 def add_cats(df, trn):
     for n, c in df.items():

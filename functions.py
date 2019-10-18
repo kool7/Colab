fbc66e0dd90e75749a4ff07fc1eeeea1c13edcd5
kool7 <40597687+kool7@users.noreply.github.com>
2019-10-18 15:10:52 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -9,3 +9,49 @@ def add_cats(df, trn):
     for n, c in df.items():
         if trn[n].dtype.name == 'category':
             df[n] = pd.Categorical(c, categories=trn[n].cat.categories, ordered=True)
+
+            
+            
+def fix_missing(df, col, name, na_dict):
+    if is_numeric_dtype(col):
+        if pd.isnull(col).sum() or (name in na_dict):
+            df[name+'_na'] = pd.isnull(col)
+            filler = na_dict[name] if name in na_dict else col.median()
+            df[name] = col.fillna(filler)
+            na_dict[name] = filler
+    return na_dict
+
+
+def proc_df(df, y_fld=None, skip_flds=None, ignore_flds=None, do_scale=False, na_dict=None,
+            preproc_fn=None, max_n_cat=None, subset=None, mapper=None):
+    if not ignore_flds: ignore_flds=[]
+    if not skip_flds: skip_flds=[]
+    if subset: df = get_sample(df,subset)
+    else: df = df.copy()
+    ignored_flds = df.loc[:, ignore_flds]
+    df.drop(ignore_flds, axis=1, inplace=True)
+    if preproc_fn: preproc_fn(df)
+    if y_fld is None: y = None
+    else:
+        if not is_numeric_dtype(df[y_fld]): df[y_fld] = pd.Categorical(df[y_fld]).codes
+        y = df[y_fld].values
+        skip_flds += [y_fld]
+    df.drop(skip_flds, axis=1, inplace=True)
+
+    if na_dict is None: na_dict = {}
+    else: na_dict = na_dict.copy()
+    na_dict_initial = na_dict.copy()
+    for n,c in df.items(): na_dict = fix_missing(df, c, n, na_dict)
+    if len(na_dict_initial.keys()) > 0:
+        df.drop([a + '_na' for a in list(set(na_dict.keys()) - set(na_dict_initial.keys()))], axis=1, inplace=True)
+    if do_scale: mapper = scale_vars(df, mapper)
+    for n,c in df.items(): numericalize(df, c, n, max_n_cat)
+    df = pd.get_dummies(df, dummy_na=True)
+    df = pd.concat([ignored_flds, df], axis=1)
+    res = [df, y, na_dict]
+    if do_scale: res = res + [mapper]
+    return res
+
+def numericalize(df, col, name, max_n_cat):
+    if not is_numeric_dtype(col) and ( max_n_cat is None or len(col.cat.categories)>max_n_cat):
+        df[name] = pd.Categorical(col).codes+1
